2d119739bef307b783b80ce3506800fdf3ab9a29
Karsten König <KarstenKoenig@linux.com>
2010-06-18 14:54:07 +0200

    Modified functiony.py; Optimized renamer.py; removed list-option 'cause renamer will automatically take notice if the last argument is a file or a directory

--- src/functions.py
+++ src/functions.py
@@ -9,6 +9,10 @@ def rename_files(start, prefix, postfix, verbose, files):
             if verbose:
                 print "Rename %s to %s" % (i,name)
             os.rename(i,name)
+            start += 1
         elif os.path.isfile(i) and os.path.exists(name):
             print "Will not rename %s to %s because %s exists!" % (i,name,name)
-        start += 1
+            start += 1
+        elif os.path.isdir(i):
+            print "Will not rename %s to %s is a directory" % (i, name)
+        
--- src/renamer.py
+++ src/renamer.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python
 
+import sys.exit
 from optparse import OptionParser
 from functions import *
 
 parser = OptionParser("Renamer.py [options] directory")
-parser.add_option("--pre",dest="prefix",default='',help="set prefix that will be placed to the left of the number")
-parser.add_option("--post",dest="postfix",default='',help="set postfix that will be placed to the right of the number")
-parser.add_option("-v","--verbose",action="store_true",dest="verbose",default=False,help="be chatty")
-parser.add_option("-s","--start", dest="start",default=1,help="set startnumber")
-parser.add_option("-l","--list",dest="list", default=False, help="use a list of files instead of all files in the directory")
+parser.add_option("--pre", dest="prefix", default='', help="set prefix that will be placed to the left of the number")
+parser.add_option("--post", dest="postfix", default='', help="set postfix that will be placed to the right of the number")
+parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="be chatty")
+parser.add_option("-s", "--start", dest="start", default=1, help="set startnumber")
 (options, args) = parser.parse_args()
 
-if len(args) == 1 and os.path.isdir(args[0]):
+if len(args) == 1:
     if options.verbose:
         print "Change directory"
     os.chdir(args[0])
     
-    files = []
-    if options.list and os.path.isfile(str(options.list)):
+    if os.path.isfile(args[0]):
         if options.verbose:
-            print "Read filenames from %s" % options.list
-        listfile = open(options.list, "r")
-        for i in listfile:
-            files.append(i.rstrip('\n'))
-    else:
+            print "Read filenames from %s" % args[0]
+        files = [i.rstrip('\n') for i in open(args[0],"r")]
+    elif os.path.isdir(args[0]):
         files = os.listdir(args[0])
-    rename_files(int(options.start),options.prefix,options.postfix,options.verbose,files)
+    elif not os.path.exists(args[0]):
+        print "File or directory don't exists!"
+        sys.exit()
+        
+    rename_files(int(options.start), options.prefix, options.postfix, options.verbose, files)
 else:
-    parser.error("Last command-line argument must be a valid path")
+    parser.error("Last command-line argument must be a valid path or a file which contain filenames")
 
