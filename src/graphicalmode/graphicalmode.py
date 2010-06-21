e71003b0db7a923752cf59375fbb284e753b2ef2
raymontag <KarstenKoenig@gmx.net>
2010-06-21 09:21:36 +0200

    start programming the layout

--- src/graphicalmode/graphicalmode.py
+++ src/graphicalmode/graphicalmode.py
@@ -1,17 +1,43 @@
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from sys import exit
 
 class MainWindow(QtGui.QWidget):
-    def __init__(self,parent=None):
-        QtGui.QWidget.__init__(self,parent)
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
         
-        self.setGeometry(300,300,250,150)
+        self.setGeometry(300, 300, 500, 500)
         self.setWindowTitle("Renamer")
+
+        #Set Data-Model for showing the directories
+        dirModel = QtGui.QFileSystemModel()
+        dirModel.setRootPath('/')
+        dirModel.setFilter(QtCore.QDir.AllDirs | QtCore.QDir.NoDotAndDotDot)#Show only directories without '.' and '..'
+        #Set the view for the directories
+        dirView = QtGui.QTreeView()
+        dirView.setModel(dirModel)
+        #Show only the directories in the view
+        dirView.setColumnHidden(1, True)
+        dirView.setColumnHidden(2, True)
+        dirView.setColumnHidden(3, True)
+        #Layout
+        dirLayout = QtGui.QVBoxLayout()
+        dirLayout.addStretch(1)
+        dirLayout.addWidget(dirView)
         
-        self.setToolTip("Hello, world!")
+        #The button to start renaming(actually it's a dummy)
+        renameButton = QtGui.QPushButton('Rename!')
+        buttonsLayout = QtGui.QHBoxLayout()
+        buttonsLayout.addStretch(1)
+        buttonsLayout.addWidget(renameButton)
         
+        vertical = QtGui.QVBoxLayout()
+        vertical.addStretch(1)
+        vertical.addLayout(dirLayout)
+        vertical.addLayout(buttonsLayout)
         
-def graphicalmode(args,options):
+        self.setLayout(vertical)
+        
+def graphicalmode(args, options):
     app = QtGui.QApplication(args)
     
     mainWindow = MainWindow()
