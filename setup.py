0203bfccbcdcf7cd2efef7bc196c98dc9ebf7bbf
Karsten König <KarstenKoenig@gmx.net>
2010-07-31 14:09:52 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
 	name = "cuteRenamer",
 	description = "A simple renamer written in Python with some nice options.",
-	version = "v.2.1",
+	version = "v.2.3",
 	license = "MIT-License",
 	author = "Karsten Koenig",
 	author_email = "KarstenKoenig@gmx.net",
