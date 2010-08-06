d375c9a0e59b7c409a6f46588efa9029a68ec559
Karsten König <KarstenKoenig@gmx.net>
2010-08-06 12:57:30 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name = "cuteRenamer",
 	description = "A simple renamer written in Python with some nice options.",
-	long_description = "A simple renamer written in Python with PySide. cuteRenamer will rename the files of a directory sequentially, that is they will be numbered serially. It has a GUI-mode and a console-mode. You need PySide to run cuteRenamer in GUI-mode.",
+	long_description = "A simple renamer written in Python with PySide. cuteRenamer will rename the files of a directory sequentially, that is they will be numbered serially. It has a GUI-mode and a console-mode. You need PyQt4 to run cuteRenamer in GUI-mode.",
 	version = "v.2.3",
 	license = "MIT-License",
 	author = "Karsten Koenig",
