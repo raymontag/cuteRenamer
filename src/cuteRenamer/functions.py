942df0313b397db0664b086c046b94953aa9bbbd
Karsten König <kkoenig@posteo.de>
2011-04-20 13:23:22 +0200

    updated copyright

--- src/cuteRenamer/functions.py
+++ src/cuteRenamer/functions.py
@@ -1,3 +1,14 @@
+'''
+This file is part of the project cuteRenamer
+
+Written by Karsten-Kai König <KKoenig@posteo.de>
+
+Copyright (C) 2011 Karsten-Kai König <KKoenig@posteo.de>
+
+This program is free software; you can use it under the terms of the MIT-License.
+You should have received a copy of the MIT-License along with this program; 
+if not check out http://www.opensource.org/licenses/mit-license.php
+'''
 import os
 import re
 
